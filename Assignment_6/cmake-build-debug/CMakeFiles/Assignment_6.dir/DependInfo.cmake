d9bb27bd909dfaf690b7516caab75907c9b38bd0
heliumyc <ericyc4@gmail.com>
2020-12-21 16:32:17 +0800

    init

---
+++ Assignment_6/cmake-build-debug/CMakeFiles/Assignment_6.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yucong/Documents/study/computer_graphics/labs/Assignment_6/src/SDLViewer.cpp" "/Users/yucong/Documents/study/computer_graphics/labs/Assignment_6/cmake-build-debug/CMakeFiles/Assignment_6.dir/src/SDLViewer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SDL_MAIN_HANDLED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ext/SDL/include"
+  "../ext/eigen"
+  "../ext/stb"
+  "../ext/gif-h"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/yucong/Documents/study/computer_graphics/labs/Assignment_6/cmake-build-debug/SDL-build/CMakeFiles/SDL2-static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Assignment_6/cmake-build-debug/CMakeFiles/RasterViewer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RasterViewer.dir/src/RasterViewer.cpp.o"
+  "CMakeFiles/RasterViewer.dir/src/raster.cpp.o"
+  "RasterViewer"
+  "RasterViewer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RasterViewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
